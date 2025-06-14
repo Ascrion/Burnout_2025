0bdb6c859b72ff4e69a76d5e12b48c86e44effa1
Ascrion <navodiths@gmail.com>
2025-06-14 09:17:07 +0530

    Tried on a different dataset

--- Navodith/main.py
+++ Navodith/main.py
@@ -1,20 +1,3 @@
 # python3 main.py
 
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-import xgboost as xgb
-import lightgbm as lgb
-from sklearn.ensemble import StackingRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-import numpy as np
-import pandas as pd
-
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
-submission = pd.read_csv("sample_submission.csv")
-
-def describer(data):
-    for i in data:
-        print(i)
-
+print(train.info)
